2723643e3b4298894010f275edf6792c9ddca4e4
tom <tomphill@live.co.uk>
2024-11-17 09:25:21 +0800

    Add property search layout, login modal, and user authentication features

--- data/properties.ts
+++ data/properties.ts
@@ -71,3 +71,22 @@ export const getPropertyById = async (propertyId: string) => {
   } as Property;
   return propertyData;
 };
+
+export const getPropertiesById = async (propertyIds: string[]) => {
+  if (!propertyIds.length) {
+    return [];
+  }
+  const propertiesSnapshot = await firestore
+    .collection("properties")
+    .where("__name__", "in", propertyIds)
+    .get();
+
+  const propertiesData = propertiesSnapshot.docs.map(
+    (doc) =>
+      ({
+        id: doc.id,
+        ...doc.data(),
+      } as Property)
+  );
+  return propertiesData;
+};
